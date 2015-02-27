91cc279fd5b8e7ec10b798d8a51ff2222374c088
Franklin Sobrinho <franklin_gs@hotmail.com>
2015-02-27 12:44:12 -0300

    New Heigthmap for experimental builder

--- Add Primitives/Core/Core.gd
+++ Add Primitives/Core/Core.gd
@@ -22,18 +22,10 @@
 tool
 extends EditorPlugin
 
-var toolbar_menu
-var popup_menu
-
 var StaticMeshBuilder
-var SettingsWindow
-var AddMeshPopup
+var Heigthmap
 
-var experimental_builder = false
-
-var current_mesh
 var mesh_instance
-
 var heigthmap
 
 #MeshData class, it's function is generate and store the mesh arrays
@@ -103,263 +95,8 @@ class MeshData:
 		self.faces += faces
 
 func _init():
-	print("PLUGIN INIT")
-	
-func _enter_tree():
-	toolbar_menu = MenuButton.new()
-	toolbar_menu.set_text('Add Mesh')
-	popup_menu = toolbar_menu.get_popup()
-	
-	update_menu()
-	
 	StaticMeshBuilder = preload("StaticMeshBuilder.gd").new()
-	
-	SettingsWindow = preload("Windows/SettingsWindow.xml").instance()
-	AddMeshPopup = preload("Windows/AddMeshPopup.xml").instance()
-	
-	add_custom_control(CONTAINER_SPATIAL_EDITOR_MENU, toolbar_menu)
-	popup_menu.connect('item_pressed', self, '_popup_signal')
-	
-func update_menu():
-	popup_menu.add_item('Add Cube')
-	popup_menu.add_item('Add Plane')
-	popup_menu.add_item('Add Cylinder')
-	popup_menu.add_item('Add Sphere')
-	popup_menu.add_item('Add Cone')
-	
-	popup_menu.add_separator()
-	#popup_menu.add_item('Immediate Geometry')
-	popup_menu.add_item('Add Heigthmap')
-	popup_menu.add_separator()
-	popup_menu.add_item('Settings')
-	
-func _popup_signal(id):
-	var command = popup_menu.get_item_text(popup_menu.get_item_index(id))
-	
-	if command == 'Add Plane':
-		if experimental_builder:
-			StaticMeshBuilder.begin(4)
-			StaticMeshBuilder.add_quad(exp_build_plane_verts(Vector3(2,0,0), Vector3(0,0,2), Vector3(-1,0,-1)),\
-			                           [Vector2(1,1), Vector2(0,0), Vector2(0,1), Vector2(1,0)], true)
-			StaticMeshBuilder.generate_normals()
-		
-			exp_add_mesh(StaticMeshBuilder.commit())
-			StaticMeshBuilder.clear()
-		
-		else:
-			var quad = build_plane(Vector3(2,0,0), Vector3(0,0,2), Vector3(-1,0,-1))
-			surface_tool(quad)
-		
-	elif command == 'Add Cube':
-		if experimental_builder:
-			var box = exp_build_box(Vector3(-1,-1,-1))
-			exp_add_mesh(box)
-		else:
-			var box = build_box()
-			surface_tool(box)
-	
-	elif command == 'Add Cylinder':
-		if experimental_builder:
-			_add_mesh_popup(AddMeshPopup, 'cylinder')
-			var cylinder = exp_build_cylinder(1, 2, 16)
-			exp_add_mesh(cylinder)
-		else:
-			var cylinder = build_cylinder(16, 2)
-			surface_tool(cylinder)
-	
-	elif command == 'Add Sphere':
-		if experimental_builder:
-			_add_mesh_popup(AddMeshPopup, 'sphere')
-			var sphere = exp_build_sphere(1, 16)
-			exp_add_mesh(sphere)
-		else:
-			pass
-	
-	elif command == 'Add Cone':
-		if experimental_builder:
-			_add_mesh_popup(AddMeshPopup, 'cone')
-			var cone = exp_build_cone(1, 2, 12, 4)
-			exp_add_mesh(cone)
-		else:
-			pass
-	
-	elif command == 'Add Heigthmap':
-		if experimental_builder:
-			_add_mesh_popup(AddMeshPopup, 'heigthmap')
-			var terrain = exp_build_heigthmap()
-			exp_add_mesh(terrain)
-		else:
-			var heigthmap = build_heigthmap()
-			surface_tool(heigthmap)
-	
-	#elif command == 'Immediate Geometry':
-	#	#Here you can add any function here to use Immediate Geometry API
-	#	var box = build_box()
-	#	immediate_geometry(box)
-	
-	elif command == 'Settings':
-		if SettingsWindow.is_hidden():
-			SettingsWindow.show()
-		elif SettingsWindow.get_parent() == null:
-			add_child(SettingsWindow)
-		elif has_node(get_path_to(SettingsWindow)):
-			remove_child(SettingsWindow)
-			add_child(SettingsWindow)
-		
-		SettingsWindow.popup_centered()
-		
-		var check_button = SettingsWindow.get_node('ExperimentalBuilder')
-		check_button.set_pressed(experimental_builder)
-		var close_button = SettingsWindow.get_node('close_button')
-		
-		if not check_button.is_connected('toggled', self, '_experimental_builder'):
-			check_button.connect('toggled', self, '_experimental_builder')
-		if not close_button.is_connected('pressed', self, '_ok_button'):
-			close_button.connect('pressed', self, '_ok_button')
-		
-#Settings
-func _experimental_builder(pressed):
-	if not pressed:
-		experimental_builder = false
-	else:
-		experimental_builder = true
-
-func _ok_button():
-	SettingsWindow.hide()
-
-#Add mesh popup
-func _update_tree_range(tree_item, text, value, _min, _max = 100, step = 1):
-	tree_item.set_text(0, text)
-	tree_item.set_cell_mode(1, 2)
-	tree_item.set_range(1, value)
-	tree_item.set_range_config(1, _min, 100.0, step)
-	tree_item.set_editable(1, true)
-
-func _add_mesh_popup(window, mesh):
-	if window.is_hidden():
-		window.show()
-	elif window.get_parent() == null:
-		add_child(window)
-	elif has_node(get_path_to(window)):
-		remove_child(window)
-		add_child(window)
-	
-	window.popup_centered()
-	var settings = window.get_node('Settings')
-	settings.clear()
-	settings.set_columns(2)
-	settings.set_column_title(0, 'Parameter')
-	settings.set_column_title(1, 'Value')
-	settings.set_column_titles_visible(true)
-	
-	var check_button = window.get_node('Smooth')
-	check_button.set_pressed(true)
-	
-	var parameters = []
-	
-	var refresh = window.get_node('Refresh')
-	if not refresh.is_connected('pressed', self, '_refresh'):
-		refresh.connect('pressed', self, '_refresh')
-	
-	if mesh == 'cylinder':
-		current_mesh = 'cylinder'
-		parameters.append(settings.create_item())
-		parameters[0].set_text(0, 'Cylinder')
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[1], 'Radius', 1, 0.1, 100, 0.1)
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[2], 'Heigth', 2, 0.1, 100, 0.1)
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[3], 'Segments', 16, 3)
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[4], 'Cuts', 1, 1)
-	
-	elif mesh == 'sphere':
-		current_mesh = 'sphere'
-		parameters.append(settings.create_item())
-		parameters[0].set_text(0, 'Sphere')
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[1], 'Radius', 1, 0.1, 100, 0.1)
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[2], 'Segments', 16, 3)
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[3], 'Cuts', 8, 3)
-	
-	elif mesh == 'cone':
-		current_mesh = 'cone'
-		parameters.append(settings.create_item())
-		parameters[0].set_text(0, 'Cone')
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[1], 'Radius', 1, 0.1, 100, 0.1)
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[2], 'Heigth', 2, 0.1, 100, 0.1)
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[3], 'Segments', 16, 3)
-	
-	elif mesh == 'heigthmap':
-		current_mesh = 'heigthmap'
-		parameters.append(settings.create_item())
-		parameters[0].set_text(0, 'Heigthmap')
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[1], 'Range', 5, 1)
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[2], 'Size', 50, 1)
-		parameters.append(settings.create_item(parameters[0]))
-		_update_tree_range(parameters[3], 'Resolution', 32, 1)
-		
-func _refresh():
-	var settings = AddMeshPopup.get_node('Settings')
-	var check_button = AddMeshPopup.get_node('Smooth')
-	
-	var smooth = check_button.is_pressed()
-	
-	var root = settings.get_root()
-	var values = []
-	var mesh_temp
-	
-	if current_mesh == 'cylinder':
-		var parameters = root.get_children()
-		values.append(parameters.get_range(1))
-		parameters = parameters.get_next()
-		values.append(parameters.get_range(1))
-		parameters = parameters.get_next()
-		values.append(parameters.get_range(1))
-		parameters = parameters.get_next()
-		values.append(parameters.get_range(1))
-		mesh_temp = exp_build_cylinder(values[0], values[1], values[2], values[3], smooth)
-	
-	elif current_mesh == 'sphere':
-		var parameters = root.get_children()
-		values.append(parameters.get_range(1))
-		parameters = parameters.get_next()
-		values.append(parameters.get_range(1))
-		parameters = parameters.get_next()
-		values.append(parameters.get_range(1))
-		mesh_temp = exp_build_sphere(values[0], values[1], values[2], smooth)
-		
-	elif current_mesh == 'cone':
-		var parameters = root.get_children()
-		values.append(parameters.get_range(1))
-		parameters = parameters.get_next()
-		values.append(parameters.get_range(1))
-		parameters = parameters.get_next()
-		values.append(parameters.get_range(1))
-		mesh_temp = exp_build_cone(values[0], values[1], values[2], smooth)
-	
-	elif current_mesh == 'heigthmap':
-		var parameters = root.get_children()
-		values.append(parameters.get_range(1))
-		parameters = parameters.get_next()
-		values.append(parameters.get_range(1))
-		parameters = parameters.get_next()
-		values.append(parameters.get_range(1))
-		mesh_temp = exp_build_heigthmap(values[1], values[2], values[0], smooth)
-	
-	if mesh_instance.get_mesh() != null:
-		mesh_instance.set_mesh(mesh_temp)
-	
-	mesh_temp == null
-	
+	Heigthmap = preload("Heigthmap/Heigthmap.gd")
 	
 #Procedual algorithms
 func exp_build_plane_verts(width_dir, length_dir, offset = Vector3(0,0,0)):
@@ -525,20 +262,30 @@ func exp_build_cone(radius, heigth, segments = 12, smooth = true):
 	
 	return mesh
 	
-func exp_build_heigthmap(size = 50, res = 32, _range = 5, smooth = true):
+func exp_build_heigthmap(_heigthmap, size = 50, res = 32, _range = 5, smooth = true):
 	var origin = Vector3(-size/2,0,-size/2)
 	var res_size = float(size)/res
 	
-	var image = preload("Heigthmap.png").get_data()
+	var image
+	
+	if _heigthmap != null:
+		image = _heigthmap.get_data()
+	else:
+		image = null
 	
 	var verts = []
 	
 	for i in range(res + 1):
 		verts.append([])
 		for j in range(res + 1):
-			var vert_heigth = image.get_pixel((image.get_height() -1) * (float(i)/res),\
-			 (image.get_width() -1) * (float(j)/res))
-			verts[i] += [Vector3(i * res_size, vert_heigth.gray() * _range, j * res_size)]
+			var vert_heigth
+			if image != null:
+				vert_heigth = image.get_pixel((image.get_height() -1) * (float(i)/res),\
+			                                      (image.get_width() -1) * (float(j)/res))
+				vert_heigth = vert_heigth.gray()
+			else:
+				vert_heigth = 0
+			verts[i] += [Vector3(i * res_size, vert_heigth * _range, j * res_size)]
 	
 	StaticMeshBuilder.begin(4)
 	StaticMeshBuilder.add_smooth_group(smooth)
@@ -558,10 +305,14 @@ func exp_build_heigthmap(size = 50, res = 32, _range = 5, smooth = true):
 	
 	StaticMeshBuilder.generate_normals()
 	var mesh = StaticMeshBuilder.commit()
+	
+	var heigthmap = Heigthmap.new()
+	heigthmap.set_mesh(mesh)
+	
 	StaticMeshBuilder.clear()
 	
-	return mesh
-	
+	return heigthmap
+
 func build_plane(width_dir, length_dir, offset = Vector3(0,0,0)):
 	var verts = []
 	verts.append(Vector3(0,0,0) + offset)
@@ -674,7 +425,7 @@ func build_heigthmap(size = 50, res = 32):
 	return mesh
 
 #Function for the next way of adding geometry,
-#it will use function Mesh.add_surface(), but it 
+#it will use function Mesh.add_surface(), but it
 #still far to be functional
 
 #func add_mesh(mesh):
@@ -704,6 +455,22 @@ func exp_add_mesh(mesh):
 		else:
 			root.add_child(mesh_instance)
 			mesh_instance.set_owner(root)
+
+func exp_add_heigthmap(heigthmap):
+	var root = get_tree().get_nodes_in_group('_viewports')[1].get_child(0)
+	
+	if root == null:
+		pass
+	else:
+		if root.get_type() != 'Spatial':
+			for node in root.get_children():
+				if node.get_type() == 'Spatial':
+					node.add_child(heigthmap)
+					mesh_instance.set_owner(heigthmap)
+					break
+		else:
+			root.add_child(heigthmap)
+			heigthmap.set_owner(root)
 
 #Immediate Geometry, still experimental
 
@@ -776,12 +543,3 @@ func surface_tool(mesh):
 		else:
 			root.add_child(mesh_instance)
 			mesh_instance.set_owner(root)
-		
-func _exit_tree():
-	SettingsWindow = null
-	AddMeshPopup = null
-	
-	popup_menu.free()
-	popup_menu = null
-	toolbar_menu.free()
-	toolbar_menu = null
---
+++ Add Primitives/ProcedualGeometry.gd
@@ -0,0 +1,333 @@
+# Copyright (c) 2015 Franklin Sobrinho.                 
+                                                                       
+# Permission is hereby granted, free of charge, to any person obtaining 
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without 
+# limitation the rights to use, copy, modify, merge, publish,   
+# distribute, sublicense, and/or sell copies of the Software, and to    
+# permit persons to whom the Software is furnished to do so, subject to 
+# the following conditions:                                             
+                                                                       
+# The above copyright notice and this permission notice shall be        
+# included in all copies or substantial portions of the Software.       
+                                                                       
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+tool
+extends "Core/Core.gd"
+
+var toolbar_menu
+var popup_menu
+
+var SettingsWindow
+var AddMeshPopup
+
+var experimental_builder = true
+
+var current_mesh
+var current_heigthmap
+
+var heigthmaps = []
+var images = []
+var counter = 0
+
+func _enter_tree():
+	toolbar_menu = MenuButton.new()
+	toolbar_menu.set_text('Add Mesh')
+	popup_menu = toolbar_menu.get_popup()
+	
+	update_menu()
+	
+	SettingsWindow = preload("Windows/SettingsWindow.xml").instance()
+	AddMeshPopup = preload("Windows/AddMeshPopup.xml").instance()
+	
+	add_custom_control(CONTAINER_SPATIAL_EDITOR_MENU, toolbar_menu)
+	popup_menu.connect('item_pressed', self, '_popup_signal')
+
+func update_menu():
+	popup_menu.add_item('Add Cube')
+	popup_menu.add_item('Add Plane')
+	popup_menu.add_item('Add Cylinder')
+	popup_menu.add_item('Add Sphere')
+	popup_menu.add_item('Add Cone')
+	
+	popup_menu.add_separator()
+	#popup_menu.add_item('Immediate Geometry')
+	popup_menu.add_item('Add Heigthmap')
+	popup_menu.add_separator()
+	popup_menu.add_item('Settings')
+
+func _popup_signal(id):
+	var command = popup_menu.get_item_text(popup_menu.get_item_index(id))
+	
+	if command == 'Add Plane':
+		if experimental_builder:
+			StaticMeshBuilder.begin(4)
+			StaticMeshBuilder.add_quad(exp_build_plane_verts(Vector3(2,0,0), Vector3(0,0,2), Vector3(-1,0,-1)),\
+			                           [Vector2(1,1), Vector2(0,0), Vector2(0,1), Vector2(1,0)], true)
+			StaticMeshBuilder.generate_normals()
+		
+			exp_add_mesh(StaticMeshBuilder.commit())
+			StaticMeshBuilder.clear()
+		
+		else:
+			var quad = build_plane(Vector3(2,0,0), Vector3(0,0,2), Vector3(-1,0,-1))
+			surface_tool(quad)
+		
+	elif command == 'Add Cube':
+		if experimental_builder:
+			var box = exp_build_box(Vector3(-1,-1,-1))
+			exp_add_mesh(box)
+		else:
+			var box = build_box()
+			surface_tool(box)
+	
+	elif command == 'Add Cylinder':
+		if experimental_builder:
+			_add_mesh_popup(AddMeshPopup, 'cylinder')
+			var cylinder = exp_build_cylinder(1, 2, 16)
+			exp_add_mesh(cylinder)
+		else:
+			var cylinder = build_cylinder(16, 2)
+			surface_tool(cylinder)
+	
+	elif command == 'Add Sphere':
+		if experimental_builder:
+			_add_mesh_popup(AddMeshPopup, 'sphere')
+			var sphere = exp_build_sphere(1, 16)
+			exp_add_mesh(sphere)
+		else:
+			pass
+	
+	elif command == 'Add Cone':
+		if experimental_builder:
+			_add_mesh_popup(AddMeshPopup, 'cone')
+			var cone = exp_build_cone(1, 2, 12, 4)
+			exp_add_mesh(cone)
+		else:
+			pass
+	
+	elif command == 'Add Heigthmap':
+		if experimental_builder:
+			_add_mesh_popup(AddMeshPopup, 'heigthmap')
+			
+			if Heigthmap:
+				heigthmaps.append(exp_build_heigthmap(null))
+				images.append(null)
+				exp_add_heigthmap(heigthmaps[counter])
+				
+				counter += 1
+				
+			if not is_processing():
+				set_process(true)
+			
+		else:
+			var heigthmap = build_heigthmap()
+			surface_tool(heigthmap)
+	
+	#elif command == 'Immediate Geometry':
+	#	#Here you can add any function here to use Immediate Geometry API
+	#	var box = build_box()
+	#	immediate_geometry(box)
+	
+	elif command == 'Settings':
+		if SettingsWindow.is_hidden():
+			SettingsWindow.show()
+		elif SettingsWindow.get_parent() == null:
+			add_child(SettingsWindow)
+		elif has_node(get_path_to(SettingsWindow)):
+			remove_child(SettingsWindow)
+			add_child(SettingsWindow)
+		
+		SettingsWindow.popup_centered()
+		
+		var check_button = SettingsWindow.get_node('ExperimentalBuilder')
+		check_button.set_pressed(experimental_builder)
+		var close_button = SettingsWindow.get_node('close_button')
+		
+		if not check_button.is_connected('toggled', self, '_experimental_builder'):
+			check_button.connect('toggled', self, '_experimental_builder')
+		if not close_button.is_connected('pressed', self, '_ok_button'):
+			close_button.connect('pressed', self, '_ok_button')
+
+#Settings
+func _experimental_builder(pressed):
+	if not pressed:
+		experimental_builder = false
+	else:
+		experimental_builder = true
+
+func _ok_button():
+	SettingsWindow.hide()
+
+#Add mesh popup
+func _update_tree_range(tree_item, text, value, _min, _max = 100, step = 1):
+	tree_item.set_text(0, text)
+	tree_item.set_cell_mode(1, 2)
+	tree_item.set_range(1, value)
+	tree_item.set_range_config(1, _min, _max, step)
+	tree_item.set_editable(1, true)
+
+func _add_mesh_popup(window, mesh):
+	if window.is_hidden():
+		window.show()
+	elif window.get_parent() == null:
+		add_child(window)
+	elif has_node(get_path_to(window)):
+		remove_child(window)
+		add_child(window)
+	
+	window.popup_centered()
+	var settings = window.get_node('Settings')
+	settings.clear()
+	settings.set_columns(2)
+	settings.set_column_title(0, 'Parameter')
+	settings.set_column_title(1, 'Value')
+	settings.set_column_titles_visible(true)
+	
+	var check_button = window.get_node('Smooth')
+	check_button.set_pressed(true)
+	
+	var parameters = []
+	
+	var refresh = window.get_node('Refresh')
+	if not refresh.is_connected('pressed', self, '_refresh'):
+		refresh.connect('pressed', self, '_refresh')
+	
+	if mesh == 'cylinder':
+		current_mesh = 'cylinder'
+		parameters.append(settings.create_item())
+		parameters[0].set_text(0, 'Cylinder')
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[1], 'Radius', 1, 0.1, 100, 0.1)
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[2], 'Heigth', 2, 0.1, 100, 0.1)
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[3], 'Segments', 16, 3)
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[4], 'Cuts', 1, 1)
+	
+	elif mesh == 'sphere':
+		current_mesh = 'sphere'
+		parameters.append(settings.create_item())
+		parameters[0].set_text(0, 'Sphere')
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[1], 'Radius', 1, 0.1, 100, 0.1)
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[2], 'Segments', 16, 3)
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[3], 'Cuts', 8, 3)
+	
+	elif mesh == 'cone':
+		current_mesh = 'cone'
+		parameters.append(settings.create_item())
+		parameters[0].set_text(0, 'Cone')
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[1], 'Radius', 1, 0.1, 100, 0.1)
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[2], 'Heigth', 2, 0.1, 100, 0.1)
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[3], 'Segments', 16, 3)
+	
+	elif mesh == 'heigthmap':
+		current_mesh = 'heigthmap'
+		parameters.append(settings.create_item())
+		parameters[0].set_text(0, 'Heigthmap')
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[1], 'Range', 5, 1)
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[2], 'Size', 50, 1)
+		parameters.append(settings.create_item(parameters[0]))
+		_update_tree_range(parameters[3], 'Resolution', 32, 1, 100, 1)
+		
+func _refresh():
+	var settings = AddMeshPopup.get_node('Settings')
+	var check_button = AddMeshPopup.get_node('Smooth')
+	
+	var smooth = check_button.is_pressed()
+	
+	var root = settings.get_root()
+	var values = []
+	var mesh_temp
+	
+	if current_mesh == 'cylinder':
+		var parameters = root.get_children()
+		values.append(parameters.get_range(1))
+		parameters = parameters.get_next()
+		values.append(parameters.get_range(1))
+		parameters = parameters.get_next()
+		values.append(parameters.get_range(1))
+		parameters = parameters.get_next()
+		values.append(parameters.get_range(1))
+		mesh_temp = exp_build_cylinder(values[0], values[1], values[2], values[3], smooth)
+	
+	elif current_mesh == 'sphere':
+		var parameters = root.get_children()
+		values.append(parameters.get_range(1))
+		parameters = parameters.get_next()
+		values.append(parameters.get_range(1))
+		parameters = parameters.get_next()
+		values.append(parameters.get_range(1))
+		mesh_temp = exp_build_sphere(values[0], values[1], values[2], smooth)
+		
+	elif current_mesh == 'cone':
+		var parameters = root.get_children()
+		values.append(parameters.get_range(1))
+		parameters = parameters.get_next()
+		values.append(parameters.get_range(1))
+		parameters = parameters.get_next()
+		values.append(parameters.get_range(1))
+		mesh_temp = exp_build_cone(values[0], values[1], values[2], smooth)
+	
+	elif current_mesh == 'heigthmap':
+		var parameters = root.get_children()
+		values.append(parameters.get_range(1))
+		parameters = parameters.get_next()
+		values.append(parameters.get_range(1))
+		parameters = parameters.get_next()
+		values.append(parameters.get_range(1))
+		mesh_temp = exp_build_heigthmap(values[1], values[2], values[0], smooth)
+	
+	if current_mesh != 'heigthmap':
+		if mesh_instance.get_mesh() != null:
+			mesh_instance.set_mesh(mesh_temp)
+	else:
+		if heigthmap.get_mesh() != null:
+			heigthmap.set_mesh(mesh_temp)
+	
+	mesh_temp == null
+
+func _process(delta):
+	var root = get_tree().get_nodes_in_group('_viewports')[1].get_child(0)
+	
+	for i in range(counter):
+		if not heigthmaps[i].is_inside_tree():
+			heigthmaps.remove(i)
+			counter -= 1
+			
+	for j in range(counter):
+		if heigthmaps[j].heigthmap != images[j]:
+			images[j] = heigthmaps[j].heigthmap
+			var heigthmap_temp = exp_build_heigthmap(heigthmaps[j].heigthmap)
+			heigthmaps[j].set_mesh(heigthmap_temp.get_mesh())
+	
+	if counter == 0:
+		print('Not processing')
+		set_process(false)
+
+func _exit_tree():
+	SettingsWindow = null
+	AddMeshPopup = null
+	
+	popup_menu.free()
+	popup_menu = null
+	toolbar_menu.free()
+	toolbar_menu = null
+	
+	set_process(false)
