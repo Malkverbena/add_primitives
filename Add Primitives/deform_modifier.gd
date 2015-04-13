9ffc7f05882c4c9a9121e3ff3255649cc8d5cf34
Franklin Sobrinho <franklin_gs@hotmail.com>
2015-04-13 10:53:05 -0300

    Fix modifiers

--- Add Primitives/deform_modifier.gd
+++ Add Primitives/deform_modifier.gd
@@ -92,20 +92,14 @@ class TaperModifier:
 		for surf in range(mesh.get_surface_count()):
 			create_from_surface(mesh, surf)
 			
-			for i in range(get_face_count()):
+			for i in range(get_vertex_count()):
 				var val = params[0]
 				
-				var v1 = get_vertex(get_face_vertex(i, 0))
-				var v2 = get_vertex(get_face_vertex(i, 1))
-				var v3 = get_vertex(get_face_vertex(i, 2))
+				var v1 = get_vertex(i)
 				
 				v1 = m3.scaled(taper(v1, val, c, axis)) * v1
-				v2 = m3.scaled(taper(v2, val, c, axis)) * v2
-				v3 = m3.scaled(taper(v3, val, c, axis)) * v3
 				
-				set_vertex(0 + (i * 3), v1)
-				set_vertex(1 + (i * 3), v2)
-				set_vertex(2 + (i * 3), v3)
+				set_vertex(i, v1)
 				
 			commit_to_surface(mesh_temp)
 			clear()
@@ -155,20 +149,14 @@ class ShearModifier:
 		for surf in range(mesh.get_surface_count()):
 			create_from_surface(mesh, surf)
 			
-			for i in range(get_face_count()):
+			for i in range(get_vertex_count()):
 				var val = params[1]
 				
-				var vert_1 = get_vertex(get_face_vertex(i, 0))
-				var vert_2 = get_vertex(get_face_vertex(i, 1))
-				var vert_3 = get_vertex(get_face_vertex(i, 2))
+				var vert_1 = get_vertex(i)
 				
 				vert_1[s_axis] += val * (vert_1[b_axis]/c)
-				vert_2[s_axis] += val * (vert_2[b_axis]/c)
-				vert_3[s_axis] += val * (vert_3[b_axis]/c)
 				
-				set_vertex(0 + (i * 3), vert_1)
-				set_vertex(1 + (i * 3), vert_2)
-				set_vertex(2 + (i * 3), vert_3)
+				set_vertex(i, vert_1)
 				
 			commit_to_surface(mesh_temp)
 			clear()
@@ -199,20 +187,14 @@ class TwistModifier:
 		for surf in range(mesh.get_surface_count()):
 			create_from_surface(mesh, surf)
 			
-			for i in range(get_face_count()):
+			for i in range(get_vertex_count()):
 				val = params[0]
 				
-				var vert_1 = get_vertex(get_face_vertex(i, 0))
-				var vert_2 = get_vertex(get_face_vertex(i, 1))
-				var vert_3 = get_vertex(get_face_vertex(i, 2))
+				var vert_1 = get_vertex(i)
 				
 				vert_1 = m3.rotated(Vector3(0,1,0), deg2rad(val * (vert_1.y/c))) * vert_1
-				vert_2 = m3.rotated(Vector3(0,1,0), deg2rad(val * (vert_2.y/c))) * vert_2
-				vert_3 = m3.rotated(Vector3(0,1,0), deg2rad(val * (vert_3.y/c))) * vert_3
 				
-				set_vertex(0 + (i * 3), vert_1)
-				set_vertex(1 + (i * 3), vert_2)
-				set_vertex(2 + (i * 3), vert_3)
+				set_vertex(i, vert_1)
 				
 			commit_to_surface(mesh_temp)
 			clear()
